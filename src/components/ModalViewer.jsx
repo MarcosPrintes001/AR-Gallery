c87d17082ae9d83b6042292aeb7d098ee5cd1b0b
MarcosPrintes001 <mvprintes2@gmail.com>
2025-05-19 22:09:13 -0300

    ajustando background do modelo 3d 2

--- src/components/ModalViewer.jsx
+++ src/components/ModalViewer.jsx
@@ -1,27 +1,34 @@
 import './ModalViewer.css'
 
-
-function ModalViewer({ produto, onClose }) {
+function ModalViewer ({ produto, onClose }) {
   return (
-    <div className="modal" onClick={e => e.target.className === 'modal' && onClose()}>
-      <button className="close-button" onClick={onClose}>×</button>
-      <div className="model-viewer-container">
+    <div
+      className='modal'
+      onClick={e => e.target.className === 'modal' && onClose()}
+    >
+      <button className='close-button' onClick={onClose}>
+        ×
+      </button>
+      <div className='model-viewer-container'>
         <model-viewer
           src={produto.glb}
           ios-src={produto.usdz}
           poster={produto.poster}
-          shadow-intensity="1"
+          shadow-intensity='1'
           camera-controls
           auto-rotate
+          style='background-color: transparent; width: 100%; height: 100%;'
           ar
           ar-placement={produto.arPlacement || 'floor'}
           alt={produto.nome}
         >
-          <button slot="ar-button" className="ar-button">👋 Ver em AR</button>
+          <button slot='ar-button' className='ar-button'>
+            👋 Ver em AR
+          </button>
         </model-viewer>
       </div>
     </div>
-  );
+  )
 }
 
 export default ModalViewer
