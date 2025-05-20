4c2af36786341fd7067ef08eaaeeb5f0666c6e8c
MarcosPrintes001 <mvprintes2@gmail.com>
2025-05-19 22:35:17 -0300

    refactor ModalViewer component to improve click handling and update background style

--- src/components/ModalViewer.jsx
+++ src/components/ModalViewer.jsx
@@ -1,10 +1,20 @@
+import { useRef } from 'react'
 import './ModalViewer.css'
 
 function ModalViewer ({ produto, onClose }) {
+  const modalRef = useRef(null)
+
+  const handleClickOutside = (e) => {
+    if (e.target === modalRef.current) {
+      onClose()
+    }
+  }
+
   return (
     <div
       className='modal'
-      onClick={e => e.target.className === 'modal' && onClose()}
+      ref={modalRef}
+      onClick={handleClickOutside}
     >
       <button className='close-button' onClick={onClose}>
         ×
@@ -16,19 +26,6 @@ function ModalViewer ({ produto, onClose }) {
           ios-src={produto.usdz}
           poster={produto.poster}
           shadow-intensity='1'
-          camera-controls
-          auto-rotate
-          style={{
-            width: '90%',
-            backgroundColor: '#000',
-            borderRadius: '8px',
-            border: 'none',
-            color: '#000',
-            fontSize: '14px',
-            padding: '8px 12px',
-            position: 'absolute',
-            top: '16px'
-          }}
           ar
           ar-placement={produto.arPlacement || 'floor'}
           alt={produto.nome}
