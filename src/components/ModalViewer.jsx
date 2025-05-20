fc54c5f8262de4bfdb612b6a5888d06465093ccc
MarcosPrintes001 <mvprintes2@gmail.com>
2025-05-19 21:57:42 -0300

    ajustando visualização

--- src/components/ModalViewer.jsx
+++ src/components/ModalViewer.jsx
@@ -10,42 +10,32 @@ function ModalViewer ({ produto, onClose }) {
         ×
       </button>
 
-      <model-viewer
-        src={produto.glb}
-        ios-src={produto.usdz}
-        poster={produto.poster}
-        shadow-intensity='1'
-        camera-controls
-        auto-rotate
-        ar
-        ar-placement={produto.arPlacement || 'floor'}
-        alt={produto.nome}
-        style={{
-          width: '90%',
-          maxWidth: 600,
-          height: 500
-        }}
+      <div
+        className='modal'
+        onClick={e => e.target.className === 'modal' && onClose()}
       >
-        <button
-          slot='ar-button'
-          style={{
-            backgroundColor: '#fff',
-            borderRadius: '8px',
-            border: 'none',
-            color: '#000',
-            fontSize: '14px',
-            padding: '8px 12px',
-            position: 'absolute',
-            top: '16px',
-            right: '16px',
-            cursor: 'pointer'
-          }}
-        >
-          👋 Ver em AR
+        <button className='close-button' onClick={onClose}>
+          ×
         </button>
-      </model-viewer>
+        <div className='model-viewer-container'>
+          <model-viewer
+            src={produto.glb}
+            ios-src={produto.usdz}
+            poster={produto.poster}
+            shadow-intensity='1'
+            camera-controls
+            auto-rotate
+            ar
+            ar-placement={produto.arPlacement || 'floor'}
+            alt={produto.nome}
+          >
+            <button slot='ar-button' className='ar-button'>
+              👋 Ver em AR
+            </button>
+          </model-viewer>
+        </div>
+      </div>
     </div>
   )
 }
 export default ModalViewer
-
