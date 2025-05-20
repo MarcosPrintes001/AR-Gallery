2d1dfe6a7e039bd8d2cc8464dc613afb58e6c200
MarcosPrintes001 <mvprintes2@gmail.com>
2025-05-19 22:18:14 -0300

    update ModalViewer component to set shadow intensity to 0 and adjust styles for better responsiveness

--- src/components/ModalViewer.jsx
+++ src/components/ModalViewer.jsx
@@ -14,21 +14,12 @@ function ModalViewer ({ produto, onClose }) {
           src={produto.glb}
           ios-src={produto.usdz}
           poster={produto.poster}
-          shadow-intensity='1'
+          shadow-intensity='0'
+          disable-zoom
           camera-controls
-          auto-rotate
-          style={{
-            width: '90%',
-            backgroundColor: '#fff',
-            borderRadius: '8px',
-            border: 'none',
-            color: '#000',
-            fontSize: '14px',
-            padding: '8px 12px',
-            position: 'absolute',
-            top: '16px'
-          }}
           ar
+          style='background-color: transparent; width: 100%; height: 100%;'
+          
           ar-placement={produto.arPlacement || 'floor'}
           alt={produto.nome}
         >
